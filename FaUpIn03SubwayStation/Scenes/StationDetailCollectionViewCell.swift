0fc54ced44d71a3402228965a7c24bb7fed0988e
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-05-09 22:00:56 +0900

    implement arrival info screen

---
+++ FaUpIn03SubwayStation/Scenes/StationDetailCollectionViewCell.swift
@@ -0,0 +1,48 @@
+//
+//  StationDetailCollectionViewCell.swift
+//  FaUpIn03SubwayStation
+//
+//  Created by joe on 5/9/24.
+//
+
+import UIKit
+import SnapKit
+
+final class StationDetailCollectionViewCell: UICollectionViewCell {
+    private lazy var lineLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 15.0, weight: .bold)
+        return label
+    }()
+    
+    private lazy var remainTimeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 15.0, weight: .medium)
+        return label
+    }()
+    
+    func setup() {
+        layer.cornerRadius = 12.0
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.2
+        layer.shadowRadius = 10
+        
+        backgroundColor = .systemBackground
+        
+        [lineLabel, remainTimeLabel].forEach { addSubview($0) }
+        
+        lineLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(16.0)
+            $0.top.equalToSuperview().inset(16.0)
+        }
+        
+        remainTimeLabel.snp.makeConstraints {
+            $0.leading.equalTo(lineLabel)
+            $0.top.equalTo(lineLabel.snp.bottom).offset(16.0)
+            $0.bottom.equalToSuperview().inset(16.0)
+        }
+        
+        lineLabel.text = "Bound for Hanyang University"
+        remainTimeLabel.text = "Arrived at Ttukseom"
+    }
+}
--- FaUpIn03SubwayStation/Scenes/StationSearchViewController.swift
+++ FaUpIn03SubwayStation/Scenes/StationSearchViewController.swift
@@ -14,6 +14,7 @@ class StationSearchViewController: UIViewController {
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.dataSource = self
+        tableView.delegate = self
         tableView.isHidden = true
         return tableView
     }()
@@ -46,12 +47,20 @@ class StationSearchViewController: UIViewController {
 extension StationSearchViewController: UISearchBarDelegate {
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         numberOfCell = 10
+        tableView.reloadData()
         tableView.isHidden = false
     }
     
     func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
         numberOfCell = 0
         tableView.isHidden = true
+    }
+}
+
+extension StationSearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = StationDetailViewController()
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 
